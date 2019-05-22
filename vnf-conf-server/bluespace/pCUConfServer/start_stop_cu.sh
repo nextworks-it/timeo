92e0c493062de64c5c3ba4fd3414e05621f0969e
j.brenes <j.brenes@nextworks.it>
2019-05-22 13:06:21 +0200

    improved start stop cu script

--- vnf-conf-server/bluespace/pCUConfServer/start_stop_cu.sh
+++ vnf-conf-server/bluespace/pCUConfServer/start_stop_cu.sh
@@ -4,12 +4,16 @@ remote_cu=$1
 
 echo `date` ": Starting CU" > /opt/VnfServer/configuration.log
 
-if [ $remote_cu -eq "1" ]; then
+if [ $remote_cu != "" ]; then
 
 	echo "Starting Central Unit"
 
 	#/home/demo/openairinterface5g/targets/bin/lte-softmodem.Rel14 -O ../P
-	sudo -E /home/demo/openairinterface5g/targets/bin/lte-softmodem.Rel14 -O /home/demo/openairinterface5g/targets/PROJECTS/GENERIC-LTE-EPC/CONF/nextworks/rcc.band7.tm1.if4p5.25PRB.usrpb210.conf
+	ip link add vxlan1 type vxlan id 2 dstport 0 remote $remote_cu
+    ifconfig vxlan1 10.255.255.1 netmask 255.255.255.0 up
+
+    sleep 2
+	screen -S cu -dm bash -c "/home/demo/openairinterface5g/targets/bin/lte-softmodem.Rel14 -O /home/demo/openairinterface5g/targets/PROJECTS/GENERIC-LTE-EPC/CONF/nextworks/rcc.band7.tm1.if4p5.25PRB.usrpb210.conf"
 else
 	echo "Stopping Central Unit"
 	sudo killall lte-softmodem.Rel14
