104dedd833bf96ee18d6bde6b384d48c9d408a13
j.brenes <j.brenes@nextworks.it>
2019-05-22 15:28:20 +0200

    changed pCU conf agent

---
+++ vnf-conf-server/bluespace/pCUConfServer/service_handlers.py
@@ -0,0 +1,46 @@
+from log import get_logger
+import netifaces
+from subprocess import Popen, PIPE, STDOUT
+import os
+
+class CUHandler:
+
+    __instance = None
+
+    def __init__(self):
+        if CUHandler.__instance is None:
+            self.logger = get_logger(__name__)
+            CUHandler.__instance = self
+            self.epc_address=""
+        else:
+            raise Exception("This class is a singleton!")
+
+    @staticmethod
+    def get_instance():
+        """ Static access method. """
+        if CUHandler.__instance is None:
+            CUHandler()
+        return CUHandler.__instance
+
+
+    def process(self, parameters):
+
+        self.logger.debug("process")
+        self.epc_address = parameters["pnf.remote.cu"]
+
+        self.__restart_service()
+
+
+
+    def __restart_service(self):
+        self.logger.debug("service restart")
+        p = Popen("/opt/pCUConfServer/start_stop_cu.sh %s" % self.epc_address, stdout=PIPE, stdin=PIPE, stderr=STDOUT, shell=True)
+        stdout = p.communicate()[0]
+        if p.returncode != 0:
+            raise Exception(stdout.decode())
+
+
+
+
+
+
